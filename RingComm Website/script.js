8f135d1803aecb5fba9d8959665e3337a923ea69
Brian Hoh <bhoh@mit.edu>
2020-01-18 17:09:15 -0500

    lol

--- RingComm Website/script.js
+++ RingComm Website/script.js
@@ -10,42 +10,12 @@
 
                  // set distance user needs to scroll before we start fadeIn
             if ($(this).scrollTop() > 1200) {
-                $('.navbar').fadeIn();
+                $('.navbar-1').fadeIn();
             } else {
-                $('.navbar').fadeOut();
+                $('.navbar-1').fadeOut();
             }
         });
     });
 
 });
   }(jQuery));
-
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
